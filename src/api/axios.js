c382dae889ea6c1eb157d9aa2b4a8ddab727a7b1
Akbar <k.akbar1101@gmail.com>
2023-01-17 14:45:58 +0600

    changed api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const instance = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL,
 });
 
+console.log(process.env.REACT_APP_BASE_URL)
   
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
